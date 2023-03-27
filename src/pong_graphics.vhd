c5e5790f015fe1817a06c6e47d6c989ed7139efa
Jonathan L. Jacobson <jonjacobson715@gmail.com>
2023-03-26 21:15:19 -0700

    Controllers are now movable. Speed may need to be adjusted slightly. Only one controller set up to move currently, but logic for the other is there.

--- src/pong_graphics.vhd
+++ src/pong_graphics.vhd
@@ -30,9 +30,11 @@ entity pong_graphics is
     xCoord        : in    coord_t;
     yCoord        : in    coord_t;
 
-    bumperCoords  : in    multiCoords_t(0 to 1); -- two bumpers
+    bumperLCoords : in    coords_t(0 to 3);      -- left bumper
+    bumperRCoords : in    coords_t(0 to 3);      -- right bumper
     midlineCoords : in    coords_t(0 to 3);      -- one midline
-    numCoords     : in    multiCoords_t(0 to 13);-- two numbers with 7 segments each
+    numLCoords    : in    multiCoords_t(0 to 6); -- left number with 7 segments
+    numRCoords    : in    multiCoords_t(0 to 6); -- right number with 7 segments
     ballCoords    : in    coords_t(0 to 3);      -- one ball (unless ...?)
 
     RED           :   out std_logic_vector(((VGA_DEPTH/3)-1) downto 0);
@@ -51,139 +53,20 @@ constant BACKGND_RGB   : std_logic_vector(VGA_DEPTH-1 downto 0) := (others => '0
 
 signal pixel_s : std_logic_vector((VGA_DEPTH-1) downto 0);
 
--- fixme. Why doesn't a generate statement work here?
-function inBumpers(x, y : coord_t;
-                   mcoords : multiCoords_t(0 to 1)
-                  ) return boolean is
-    variable isIn : boolean := false;
-  begin
-    -- check if within coords of each of the 2 bumpers
-    if(   x >= mcoords(0)(0)  and
-          x <= mcoords(0)(1)  and
-          y >= mcoords(0)(2)  and
-          y <= mcoords(0)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(1)(0)  and
-          x <= mcoords(1)(1)  and
-          y >= mcoords(1)(2)  and
-          y <= mcoords(1)(3)) then 
-      isIn := true;
-    end if;
-    return isIn;
-  end function;
-
-  -- fixme. Why doesn't a generate statement work here?
-  function inNumbers(x, y : coord_t;
-                     mcoords : multiCoords_t(0 to 13)
-                    ) return boolean is
-    variable isIn : boolean := false;
-  begin
-    -- check if within coords of each segment of the 2 7 segment numbers
-    if(   x >= mcoords(0)(0)  and
-          x <= mcoords(0)(1)  and
-          y >= mcoords(0)(2)  and
-          y <= mcoords(0)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(1)(0)  and
-          x <= mcoords(1)(1)  and
-          y >= mcoords(1)(2)  and
-          y <= mcoords(1)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(2)(0)  and
-          x <= mcoords(2)(1)  and
-          y >= mcoords(2)(2)  and
-          y <= mcoords(2)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(3)(0)  and
-          x <= mcoords(3)(1)  and
-          y >= mcoords(3)(2)  and
-          y <= mcoords(3)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(4)(0)  and
-          x <= mcoords(4)(1)  and
-          y >= mcoords(4)(2)  and
-          y <= mcoords(4)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(5)(0)  and
-          x <= mcoords(5)(1)  and
-          y >= mcoords(5)(2)  and
-          y <= mcoords(5)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(6)(0)  and
-          x <= mcoords(6)(1)  and
-          y >= mcoords(6)(2)  and
-          y <= mcoords(6)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(7)(0)  and
-          x <= mcoords(7)(1)  and
-          y >= mcoords(7)(2)  and
-          y <= mcoords(7)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(8)(0)  and
-          x <= mcoords(8)(1)  and
-          y >= mcoords(8)(2)  and
-          y <= mcoords(8)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(9)(0)  and
-          x <= mcoords(9)(1)  and
-          y >= mcoords(9)(2)  and
-          y <= mcoords(9)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(10)(0)  and
-          x <= mcoords(10)(1)  and
-          y >= mcoords(10)(2)  and
-          y <= mcoords(10)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(11)(0)  and
-          x <= mcoords(11)(1)  and
-          y >= mcoords(11)(2)  and
-          y <= mcoords(11)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(12)(0)  and
-          x <= mcoords(12)(1)  and
-          y >= mcoords(12)(2)  and
-          y <= mcoords(12)(3)) then 
-      isIn := true;
-    elsif(x >= mcoords(13)(0)  and
-          x <= mcoords(13)(1)  and
-          y >= mcoords(13)(2)  and
-          y <= mcoords(13)(3)) then 
-      isIn := true;
-    end if;
-
-    return isIn;
-  end function;
-
-  function inMidline(x, y : coord_t;
-                      coords : coords_t(0 to 3)
-                    ) return boolean is
-    variable isIn : boolean := false;
-  begin
-    if( x >= coords(0)  and 
-        x <= coords(1)  and 
-        y >= coords(2)  and 
-        y <= coords(3)) then
-      isIn := true;
-    end if;
-
-    return isIn;
-  end function;
-
-  function inBall (x, y : coord_t;
-                   coords : coords_t(0 to 3)
-                  ) return boolean is
-    variable isIn : boolean := false;
-  begin
-    -- check each segment of the 7 segment display
-    if( x >= coords(0)  and 
-        x <= coords(1)  and 
-        y >= coords(2)  and 
-        y <= coords(3)) then 
-      isIn := true;
-    end if;
-
-    return isIn;
-  end function;
+function inNumbers(x, y: coord_t; mcoords: multiCoords_t(0 to 6)) return boolean is
+  variable isIn : boolean := false;
+begin
+  if(inCoords(x, y, mcoords(0)) or
+     inCoords(x, y, mcoords(1)) or
+     inCoords(x, y, mcoords(2)) or
+     inCoords(x, y, mcoords(3)) or
+     inCoords(x, y, mcoords(4)) or
+     inCoords(x, y, mcoords(5)) or
+     inCoords(x, y, mcoords(6))) then
+    isIn := true;
+  end if;
+  return isIn;
+end function;
 
 begin
 
@@ -192,19 +75,18 @@ begin
   GREEN <= pixel_s((((VGA_DEPTH/3)*2)-1) downto  (VGA_DEPTH/3));    -- (((12/3)*2)-1) =  7
   BLUE  <= pixel_s((VGA_DEPTH-1)         downto ((VGA_DEPTH/3)*2)); --    12-1        = 11
 
-  
-  graphics_proc : process (clk, rst, en, xCoord, yCoord)
+  graphics_proc : process (clk, rst, en)
   begin
     if(rst = '1') then
       pixel_s <= (others => '0');
     elsif(rising_edge(clk) and (en = '1')) then
-      if(   inBumpers(xCoord, yCoord, bumperCoords)) then
+      if(inCoords(xCoord, yCoord, bumperLCoords) or inCoords(xCoord, yCoord, bumperRCoords)) then
         pixel_s <= BUMPERS_RGB;
-      elsif(inMidline(xCoord, yCoord, midlineCoords)) then
+      elsif(inCoords(xCoord, yCoord, midlineCoords)) then
         pixel_s <= MIDLINE_RGB;
-      elsif(inNumbers(xCoord, yCoord, numCoords)) then
+      elsif(inNumbers(xCoord, yCoord, numLCoords) or inNumbers(xCoord, yCoord, numRCoords)) then
         pixel_s <= NUM_RGB;
-      elsif(inBall(xCoord, yCoord, ballCoords)) then
+      elsif(inCoords(xCoord, yCoord, ballCoords)) then
         pixel_s <= BALL_RGB;
       else
         pixel_s <= BACKGND_RGB;
--- src/pong_logic.vhd
+++ src/pong_logic.vhd
@@ -20,19 +20,26 @@ library jacobson_ip;
 
 entity pong_logic is
   generic (
-    PADDLEL_SIZE  : integer  := NORMAL; -- XSMALL, SMALL, NORMAL, LARGE, FULL
-    PADDLER_SIZE  : integer  := NORMAL; -- XSMALL, SMALL, NORMAL, LARGE, FULL
     hVisibleArea  : integer := 640;
-    vVisibleArea  : integer := 480
+    vVisibleArea  : integer := 480;
+    PADDLEL_SIZE  : integer := NORMAL; -- XSMALL, SMALL, NORMAL, LARGE, FULL
+    PADDLER_SIZE  : integer := NORMAL; -- XSMALL, SMALL, NORMAL, LARGE, FULL
+    BUMPER_SPEED  : integer := 20;
+    BALL_SPEED    : integer := 30
   );
   port (
     clk           : in    std_logic;
     rst           : in    std_logic;
     en            : in    std_logic;
 
-    bumperCoords  :   out multiCoords_t(0 to 1);
+    controlLIn    : in    std_logic_vector(0 to 3);
+    controlRIn    : in    std_logic_vector(0 to 3);
+
+    bumperLCoords :   out coords_t(0 to 3);
+    bumperRCoords :   out coords_t(0 to 3);
     midlineCoords :   out coords_t(0 to 3);
-    numCoords     :   out multiCoords_t(0 to 13);
+    numLCoords    :   out multiCoords_t(0 to 6);
+    numRCoords    :   out multiCoords_t(0 to 6);
     ballCoords    :   out coords_t(0 to 3);
 
     scoreL        :   out integer;
@@ -40,73 +47,113 @@ entity pong_logic is
   );
 end entity pong_logic;
 architecture RTL of pong_logic is
-  -- \(\(5\*hVisibleArea/8\)\)
-  signal bumperCoords_s  : multiCoords_t(0 to 1) := 
-      ((toSLV(10),  toSLV(20),  toSLV((vVisibleArea/2) - (PADDLEL_SIZE/2)),  toSLV((vVisibleArea/2) + (PADDLEL_SIZE/2))),
-       (toSLV(hVisibleArea-30), toSLV(hVisibleArea-20), toSLV((vVisibleArea/2) - (PADDLER_SIZE/2)),  toSLV((vVisibleArea/2) + (PADDLER_SIZE/2))));
+  signal scoreL_s : integer := 0;
+  signal scoreR_s : integer := 0;
+
+  signal bumperLCoords_s  : coords_t(0 to 3) := 
+      (toSLV(10),  toSLV(20),  toSLV((vVisibleArea/2) - (PADDLEL_SIZE/2)),  toSLV((vVisibleArea/2) + (PADDLEL_SIZE/2)));
+
+  signal bumperRCoords_s  : coords_t(0 to 3) :=
+       (toSLV(hVisibleArea-30), toSLV(hVisibleArea-20), toSLV((vVisibleArea/2) - (PADDLER_SIZE/2)),  toSLV((vVisibleArea/2) + (PADDLER_SIZE/2)));
 
   signal midlineCoords_s : coords_t(0 to 3) := 
       (toSLV((hVisibleArea/2)-1),  toSLV((hVisibleArea/2)+1),  toSLV(0),  toSLV(vVisibleArea));
 
-  signal numCoords_s     : multiCoords_t(0 to 13) := 
-       -- left number (starts at top, goes CW, then middle) (this is '8')
-      ((toSLV(((3*hVisibleArea)/8)-20), toSLV(((3*hVisibleArea)/8)),    toSLV(20),  toSLV(24)),
-       (toSLV(((3*hVisibleArea)/8)-4),  toSLV(((3*hVisibleArea)/8)),    toSLV(20),  toSLV(40)),
-       (toSLV(((3*hVisibleArea)/8)-4),  toSLV(((3*hVisibleArea)/8)),    toSLV(41),  toSLV(61)),
-       (toSLV(((3*hVisibleArea)/8)-20), toSLV(((3*hVisibleArea)/8)),    toSLV(57),  toSLV(61)),
-       (toSLV(((3*hVisibleArea)/8)-20), toSLV(((3*hVisibleArea)/8)-16), toSLV(41),  toSLV(61)),
-       (toSLV(((3*hVisibleArea)/8)-20), toSLV(((3*hVisibleArea)/8)-16), toSLV(20),  toSLV(40)),
-       (toSLV(((3*hVisibleArea)/8)-20), toSLV(((3*hVisibleArea)/8)), toSLV(38),  toSLV(42)),
+  signal numLCoords_s     : multiCoords_t(0 to 6) := 
+       intToPixels(0, (toSLV(((3*hVisibleArea)/8)-20), toSLV(((3*hVisibleArea)/8)),    toSLV(20),  toSLV(61)));
 
-       -- right number (starts at top, goes CW, then middle) (this is '8')
-       (toSLV((5*hVisibleArea)/8),      toSLV(((5*hVisibleArea)/8)+20), toSLV(20),  toSLV(24)),
-       (toSLV(((5*hVisibleArea)/8)+16), toSLV(((5*hVisibleArea)/8)+20), toSLV(20),  toSLV(40)),
-       (toSLV(((5*hVisibleArea)/8)+16), toSLV(((5*hVisibleArea)/8)+20), toSLV(41),  toSLV(61)),
-       (toSLV((5*hVisibleArea)/8),      toSLV(((5*hVisibleArea)/8)+20), toSLV(57),  toSLV(61)),
-       (toSLV((5*hVisibleArea)/8),      toSLV((5*hVisibleArea/8)+4),    toSLV(41),  toSLV(61)),
-       (toSLV((5*hVisibleArea)/8),      toSLV((5*hVisibleArea/8)+4),    toSLV(20),  toSLV(40)),
-       (toSLV((5*hVisibleArea)/8),      toSLV(((5*hVisibleArea)/8)+20), toSLV(38),  toSLV(42)));
-       --(toSLV(hVisibleArea+1),          toSLV(hVisibleArea+1), toSLV(vVisibleArea+1),  toSLV(vVisibleArea+1)));
+  signal numRCoords_s     : multiCoords_t(0 to 6) := 
+       intToPixels(0, (toSLV((5*hVisibleArea)/8),       toSLV(((5*hVisibleArea)/8)+20), toSLV(20),  toSLV(61)));
 
   signal ballCoords_s    : coords_t(0 to 3) := 
       (toSLV(10),  toSLV(20),  toSLV(30),  toSLV(40));
 
+  signal controllerLdirection : std_logic_vector(3 downto 0) := "0000";
+  signal controllerRdirection : std_logic_vector(3 downto 0) := "0000";
+  signal ballStatus           : std_logic_vector(3 downto 0) := "0000";
+
+  signal ballDirection : std_logic_vector(3 downto 0) := "0000";
+
+  signal bumperSpeedControl : std_logic := '0';
+  signal ballSpeedControl   : std_logic := '0';
 begin
 
-  --reset_proc : process(clk, rst)
-  --begin
-  --  if(rst = '1') then
-  --    bumperCoords_s(0)(0) <= std_logic_vector(to_unsigned(10,  bumperCoords_s(0)(0)'length));
-  --    bumperCoords_s(0)(1) <= std_logic_vector(to_unsigned(20,  bumperCoords_s(0)(1)'length));
-  --    bumperCoords_s(0)(2) <= std_logic_vector(to_unsigned(30,  bumperCoords_s(0)(2)'length));
-  --    bumperCoords_s(0)(3) <= std_logic_vector(to_unsigned(50,  bumperCoords_s(0)(3)'length));
+  reset_proc : process(clk, rst)
+  begin
+    if(rst = '1') then
+      scoreL_s <= 0;
+      scoreR_s <= 0;
+      bumperLCoords_s <=
+       (toSLV(10),  toSLV(20),  toSLV((vVisibleArea/2) - (PADDLEL_SIZE/2)),  toSLV((vVisibleArea/2) + (PADDLEL_SIZE/2)));
+      bumperRCoords_s <=
+       (toSLV(hVisibleArea-30), toSLV(hVisibleArea-20), toSLV((vVisibleArea/2) - (PADDLER_SIZE/2)),  toSLV((vVisibleArea/2) + (PADDLER_SIZE/2)));
+      midlineCoords_s <=
+      (toSLV((hVisibleArea/2)-1),  toSLV((hVisibleArea/2)+1),  toSLV(0),  toSLV(vVisibleArea));
+      numLCoords_s    <=
+       intToPixels(0, (toSLV(((3*hVisibleArea)/8)-20), toSLV(((3*hVisibleArea)/8)),    toSLV(20),  toSLV(61)));
+      numRCoords_s    <=
+       intToPixels(0, (toSLV((5*hVisibleArea)/8),      toSLV(((5*hVisibleArea)/8)+20), toSLV(20),  toSLV(61)));
+      ballCoords_s   <=
+       (toSLV(hVisibleArea+1), toSLV(hVisibleArea+1), toSLV(vVisibleArea+1),  toSLV(vVisibleArea+1));
 
-  --    bumperCoords_s(1)(0) <= std_logic_vector(to_unsigned(100, bumperCoords_s(1)(0)'length));
-  --    bumperCoords_s(1)(1) <= std_logic_vector(to_unsigned(110, bumperCoords_s(1)(1)'length));
-  --    bumperCoords_s(1)(2) <= std_logic_vector(to_unsigned(130, bumperCoords_s(1)(2)'length));
-  --    bumperCoords_s(1)(3) <= std_logic_vector(to_unsigned(150, bumperCoords_s(1)(3)'length));
-  --  end if;
-  --end process;
-  -- setup Left Bumper
-  
-  -- setup Right Bumper
+    elsif(rising_edge(clk) and en = '1') then
+      -- Left Bumper
+        bumperLCoords_s <= shiftCoords(controllerLdirection, bumperLCoords_s);
+      -- Right Bumper
+        bumperRCoords_s <= shiftCoords(controllerRdirection, bumperRCoords_s); 
+      -- Ball
+        ballCoords_s <= shiftCoords(ballDirection, ballCoords_s);
+      -- Left Number
+        numLCoords_s <= intToPixels(scoreL_s, (toSLV(((3*hVisibleArea)/8)-20), toSLV(((3*hVisibleArea)/8)),    toSLV(20),  toSLV(61)));
+      -- Right Number
+        numRCoords_s <= intToPixels(scoreR_s, (toSLV((5*hVisibleArea)/8),      toSLV(((5*hVisibleArea)/8)+20), toSLV(20),  toSLV(61)));
+    end if;
+  end process;
 
-  -- set midline
+  bumper_speed_inst : entity jacobson_ip.ip_counter(RTL)
+  generic map (
+    START_VAL  => 0,
+    STOP_VAL   => BUMPER_SPEED,
+    LOOP_IN    => '1'
+  )
+  port map (
+    clk      => clk,
+    rst      => rst,
+    enableIn => en,
+    countOut => open,
+    doneOut  => bumperSpeedControl
+  );
 
-  -- set Ball
+  ball_speed_inst : entity jacobson_ip.ip_counter(RTL)
+  generic map (
+    START_VAL  => 0,
+    STOP_VAL   => BALL_SPEED,
+    LOOP_IN    => '1'
+  )
+  port map (
+    clk      => clk,
+    rst      => rst,
+    enableIn => en,
+    countOut => open,
+    doneOut  => ballSpeedControl
+  );
 
+  controllerLdirection(0)   <= bumperSpeedControl when controlLIn(0) = '1' else '0';
+  controllerLdirection(1)   <= bumperSpeedControl when controlLIn(1) = '1' else '0';
+  controllerRdirection(0)   <= bumperSpeedControl when controlRIn(0) = '1' else '0';
+  controllerRdirection(1)   <= bumperSpeedControl when controlRIn(1) = '1' else '0';
+  ballDirection(0) <= ballSpeedControl when ballStatus(0) = '1' else '0';
+  ballDirection(1) <= ballSpeedControl when ballStatus(1) = '1' else '0';
+  ballDirection(2) <= ballSpeedControl when ballStatus(2) = '1' else '0';
+  ballDirection(3) <= ballSpeedControl when ballStatus(3) = '1' else '0';
 
-
-
-  -- worry about these later
-  -- set Left Number
-  -- set Right Number
-
-  bumperCoords  <= bumperCoords_s;
+  bumperLCoords <= bumperLCoords_s;
+  bumperRCoords <= bumperRCoords_s;
   midlineCoords <= midlineCoords_s;
-  numCoords     <= numCoords_s;
+  numLCoords    <= numLCoords_s;
+  numRCoords    <= numRCoords_s;
   ballCoords    <= ballCoords_s;
-  scoreL        <= 0;
-  scoreR        <= 0;
+  scoreL        <= scoreL_s;
+  scoreR        <= scoreR_s;
 
 end architecture RTL;
